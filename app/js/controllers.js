f81245aef961c8fc18ea91c8e18eeda61074ed5f
Joshua <joshua.hyk@gmail.com>
2013-12-11 14:58:31 -0600

    sort receipts based on date

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -268,6 +268,14 @@ getpaidControllers.controller('ReceiptListCtrl', ['$scope', '$location','receipt
 				i--;
 			}
 			console.log(outputarr);
+			var date_sort_desc = function (date1, date2) {
+  // This is a comparison function that will result in dates being sorted in
+  // DESCENDING order.
+  if (date1.receiptDate > date2.receiptDate) return -1;
+  if (date1.receiptDate < date2.receiptDate) return 1;
+  return 0;
+};
+			outputarr.sort(date_sort_desc);
 			$scope.receipts = outputarr;
 			$scope.username = localStorage.username.split(" ")[0];
 			$scope.total = getTotal(data).toFixed(2);
